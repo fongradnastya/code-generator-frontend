b462a95dac0105fba4e57129f5255298f193a0f3
Anastasia Fongrad <fongradnastya@gmail.com>
2025-03-05 22:25:49 +0700

    Create a project dto and an upload result dto

---
+++ src/api/dtos/projectDto.ts
@@ -0,0 +1,27 @@
+import { type ProjectStatusDto } from './projectStatusDto';
+import { type ProjectTypeDto } from './projectTypeDto';
+
+/** 1. */
+export type ProjectDto = {
+
+  /** 1. */
+  readonly project_name: string;
+
+  /** 1. */
+  readonly description: string;
+
+  /** 1. */
+  readonly project_type: ProjectTypeDto;
+
+  /** 1. */
+  readonly project_status: ProjectStatusDto;
+
+  /** 1. */
+  readonly file_name: string;
+
+  /** 1. */
+  readonly file_id: string;
+
+  /** 1. */
+  readonly created_at: string;
+};
---
+++ src/api/dtos/uploadResultDto.ts
@@ -0,0 +1,12 @@
+/** 1. */
+export type UploadResultDto = {
+
+  /** 1. */
+  readonly file_id: string;
+
+  /** 1. */
+  readonly file_name: string;
+
+  /** 1. */
+  readonly project_id: number;
+};
