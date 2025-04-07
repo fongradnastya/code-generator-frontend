5eb1cf2a3b560db81676b9cbad93957594c7f13f
Anastasia Fongrad <fongradnastya@gmail.com>
2025-04-07 19:59:27 +0700

    Remove a default process template page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,6 @@ import { setOpen } from 'src/store/drawer/slice';
 import { type NavigationProps } from 'src/utils/types/navigationProps';
 import { useAppSelector, useAppDispatch } from 'src/store';
 import HomeIcon from '@mui/icons-material/Home';
-import SettingsIcon from '@mui/icons-material/Settings';
 import CreateIcon from '@mui/icons-material/Create';
 import LoginIcon from '@mui/icons-material/Login';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
@@ -27,7 +26,6 @@ const DRAWER_WIDTH = 280;
 
 const mainRoutes: NavigationProps[] = [
   { name: 'My Templates', path: '/templates', icon: HomeIcon },
-  { name: 'Process Template', path: '/process', icon: SettingsIcon },
   { name: 'Create Template', path: '/create', icon: CreateIcon },
 ];
 
